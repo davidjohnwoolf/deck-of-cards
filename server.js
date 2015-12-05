e5d6195880b877f130990ad64b6e8ba5ee676e67
David Woolf <davidjohnwoolf@gmail.com>
2015-12-05 11:45:59 -0800

    add server file

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 1337);
+
+app.use(express.static(__dirname + '/'));
+
+// get index
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+// server
+app.listen(app.get('port'));
+console.log('Listening on 1337 in development...');
